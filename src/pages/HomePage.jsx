fe05d29f4fb79a25cc70f4477d7ac016402c3ea2
LJ1319 <luka.jikia.2@btu.edu.ge>
2023-02-09 09:51:04 +0400

    add minor home page style changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import logo02 from "../assets/img/logo02.png";
 export default function HomePage() {
   return (
     <div className="h-screen w-screen bg-home-hero bg-cover bg-center bg-no-repeat">
-      <div className="mx-auto w-11/12 border-b-[1px] border-black py-6">
+      <div className="mx-auto w-11/12 border-b-[1px] border-[#1A1A1A] py-6">
         <img src={logo01} alt="redberry" />
       </div>
       <div className="my-96 mx-auto flex w-11/12 flex-col items-center">
@@ -14,8 +14,13 @@ export default function HomePage() {
           alt="redberry-transformation-agency"
           className="absolute left-[1065px] top-[430px]"
         />
-        <button className="z-10 h-16 w-3/12 rounded-lg  bg-black text-center font-helvetica-neue text-3xl text-white all-small-caps">
-          <Link to="build">რეზიუმეს დამატება</Link>
+        <button className="z-10 h-16 w-3/12 rounded-lg bg-[#1A1A1A]">
+          <Link
+            to="build"
+            className="block text-center font-helvetica-neue text-3xl text-white all-small-caps"
+          >
+            რეზიუმეს დამატება
+          </Link>
         </button>
       </div>
     </div>
