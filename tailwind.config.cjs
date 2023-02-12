f0e977f8e0fac80a3b815c91a49c2a61ef28a59b
LJ1319 <luka.jikia.2@btu.edu.ge>
2023-02-12 08:53:41 +0400

    fix: optimize tailwind for production

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./index.html",
-    // "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
